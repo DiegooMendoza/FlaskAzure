c1470dd8fca63681c4fced7e7b6b983defd77362
derdiego91 <der_diego@hotmail.com>
2022-07-07 14:13:24 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,21 @@
 from datetime import datetime
-from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory, Response
+import jsonpickle
+import numpy as np
+import cv2
+
+import imutils
+
+from scipy.spatial import distance as dist
+from tkinter import *
+from tkinter import filedialog
+from PIL import Image
+from PIL import ImageTk
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.preprocessing.image import load_img
+from tensorflow.keras.models import load_model
+
+
 app = Flask(__name__)
 
 
@@ -7,6 +23,26 @@ app = Flask(__name__)
 def index():
    print('Request for index page received')
    return render_template('index.html')
+
+@app.route('/api', methods=['POST'])
+def test():
+    r = request
+    # convert string of image data to uint8
+    nparr = np.fromstring(r.data, np.uint8)
+    # decode image
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+
+    image = img_to_array(img) / 255.0
+ 
+    # build a response dict to send back to client
+    response = {'message': 'image received. size={}x{}'.format(img.shape[1], img.shape[0])
+
+    response_pickled = jsonpickle.encode(response)
+
+    return Response(response=response_pickled, status=200, mimetype="application/json")
+
+def midpoint(ptA, ptB):
+	return ((ptA[0] + ptB[0]) * 0.5, (ptA[1] + ptB[1]) * 0.5)
 
 @app.route('/favicon.ico')
 def favicon():
